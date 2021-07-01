8ab503699358ab3ca9cafb064b2bb18edf960e46
Denise Welsch <denise.welsch@reyar.de>
2021-07-01 10:31:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MyApp_R
+ For contributors: Container for new Statsomat app with R 
---
+++ global.R
@@ -0,0 +1,12 @@
+MAX_FILE_SIZE_MB <- 1L
+options(shiny.maxRequestSize = MAX_FILE_SIZE_MB*1024^2)
+options(shiny.sanitize.errors = TRUE)
+
+library(shiny)
+library(rmarkdown)
+library(data.table)
+library(readr)
+library(shinydisconnect)
+
+source("chooser.R")
+source("helpers/Functions.R")
